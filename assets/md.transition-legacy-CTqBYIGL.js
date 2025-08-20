8b78cd3615ffbdf30ae8a42a9dedc28d143f4866
user4441102 <user4441102@outlook.com>
2025-08-21 04:01:07 +0500

    Updates

--- assets/md.transition-legacy-CTqBYIGL.js
+++ assets/md.transition-legacy-CTqBYIGL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1zYXPsIb.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BP86_1WE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
