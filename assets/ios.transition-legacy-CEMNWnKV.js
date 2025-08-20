6e4be436195605feb6638968b7962aa8be4978ec
user4441102 <user4441102@outlook.com>
2025-08-21 04:31:32 +0500

    Updates

--- assets/ios.transition-legacy-CEMNWnKV.js
+++ assets/ios.transition-legacy-CEMNWnKV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BP86_1WE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-ChmcJI9J.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
